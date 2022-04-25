921b358362c52a073dc5b6978e8985e25f00aff0
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-25 13:34:54 +0530

    [Harshal]Add:OOPS Json File Project

---
+++ OOPS/Program.cs
@@ -0,0 +1,31 @@
+﻿using OOPS.InventoryDataManagement;
+using System;
+namespace OOPS
+{
+    class program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Welcome To Inventory Management");
+            const string INVENTORY_DATA_PATH = @"E:\VisualPractise\OOPS\InventoryDataManagement\Inventory.json";
+            InventoryManagement inventory = new InventoryManagement();
+            Console.WriteLine("Display\n");
+            bool check = true;
+            while(check)
+            {
+                Console.WriteLine("Enter The Option");
+                int option=Convert.ToInt32(Console.ReadLine()); 
+                switch(option)
+                {
+                    case 1:
+                        inventory.DisplayInventory(INVENTORY_DATA_PATH);
+                        break;
+                    case 2:
+                        check=false;
+                        break;
+;                }
+            }
+        }
+    }
+
+}
